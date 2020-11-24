036cc750d3c31a99b742de889ed15f5298c4cc65
nbelaribi <you@example.com>
2020-11-24 10:46:38 +0100

    change colors

--- src/Models/User.php
+++ src/Models/User.php
@@ -22,4 +22,31 @@ class User
     {
         return $this->password;
     }
+
+  /*  public function checkLogin ()
+    {
+        $users = $this->pdo->prepare('select * from users where email = :mail ');
+        $users->bindParam(':mail', $this->email, PDO::PARAM_STR);
+        $users->execute() ;
+        if ($users->rowCount() == 1)
+        {
+           $user = $users->fetch() ;
+           if($user['password'] == base64_decode($this->password))
+               return $user['id'] ;
+        }
+        return -1 ;
+    }
+
+    public function userExists(string $username): bool {
+        $query = 'SELECT 1 FROM users WHERE username = ?';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([$username]);
+        $res = $stmt->fetch();
+
+        if (!$res) {
+            return false;
+        }
+
+        return true;
+    }*/
 }
---
+++ tests/UserTest.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Amir_nadjib\Todo_list\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Amir_nadjib\Todo_list\Models\User;
+
+class UserTest extends TestCase {
+}
